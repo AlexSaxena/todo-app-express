b6b99eb63b5614c06ce7cfae319f0b451e56bcbe
AlexSaxena <saxenaalex@gmail.com>
2023-03-09 18:07:02 +0100

    Adds server require packages & DB Config

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 console.log("General Kenobi");
+
+// Require / Imports
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+require("dotenv").config();
+
+// Configurations / Definitions
+const config = {
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_DATABASE,
+};
+const app = express();
