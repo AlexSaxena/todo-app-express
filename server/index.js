3a723283b697afa7257a8568b89d290e03cbc8f6
AlexSaxena <saxenaalex@gmail.com>
2023-03-13 16:53:16 +0100

    creates pool & boiler for post/get

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const mysql = require("mysql2");
 const cors = require("cors");
 require("dotenv").config();
 
-// Configurations / Definitions
+// MySql Configurations / Definitions
 const config = {
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
@@ -14,3 +14,23 @@ const config = {
   database: process.env.DATABASE_DATABASE,
 };
 const app = express();
+
+// Creates pool for MySql Connections
+const pool = mysql.createPool(config);
+
+// Code - Functionality
+
+// POST Register
+app.post("/register", (req, res) => {});
+
+// POST Login
+app.post("/login", (req, res) => {});
+
+// POST Add ToDo
+app.post("/addTodo", (req, res) => {});
+
+// GET Show All ToDos
+app.get("/", (req, res) => {});
+
+// Server Port
+app.listen(5050);
