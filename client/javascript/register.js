d4e61087c96a5efe21e6a536c1545e47f5e04810
AlexSaxena <saxenaalex@gmail.com>
2023-03-19 16:15:24 +0100

    Adds Fetch -> POST req for userRegistration

---
+++ client/javascript/register.js
@@ -0,0 +1,29 @@
+console.log("Hello from register.js");
+
+let registerForm = document.querySelector(".register-form");
+let inputUsername = document.querySelector("#username");
+let inputPassword = document.querySelector("#password");
+let formRegisterBtn = document.querySelector(".register-form-btn");
+let responseOutput = document.querySelector(".h3-response");
+
+registerForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  let username = inputUsername.value;
+  let password = inputPassword.value;
+
+  const response = await fetch("http://localhost:5050/register", {
+    method: "POST",
+    body: JSON.stringify({ username, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  const data = await response.json();
+  console.log("Data ->", data);
+  console.log("response ->", response);
+
+  responseOutput.textContent = data.message;
+});
