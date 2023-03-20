eed201326660fb030cb6931bbc241ee0550e520b
AlexSaxena <saxenaalex@gmail.com>
2023-03-20 23:31:23 +0100

    Adds Fetch POST request for adding Friend

--- client/javascript/friend.js
+++ client/javascript/friend.js
@@ -7,6 +7,23 @@ let responseOutput = document.querySelector(".h3-response");
 
 formRegisterBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(inputUsername.value);
-  responseOutput.textContent = `Added ${inputUsername.value}`;
+
+  let username = inputUsername.value;
+  // responseOutput.textContent = `Added ${username}`;
+  addFriend(username);
 });
+
+async function addFriend(username) {
+  const response = await fetch("http://localhost:5050/friends", {
+    method: "POST",
+    body: JSON.stringify({ username }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  const data = await response.json();
+  responseOutput.textContent = data.message;
+  console.log("Add Todo data -> ", data);
+}
