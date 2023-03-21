fda94bcec035a71fc9051b58de4080838005dacb
AlexSaxena <saxenaalex@gmail.com>
2023-03-21 17:33:42 +0100

    Adds getFunction for FriendTodos

--- server/controllers/friendController/getFriend.js
+++ server/controllers/friendController/getFriend.js
@@ -10,6 +10,29 @@ const config = {
 
 const pool = mysql.createPool(config);
 
-const getFriend = function getFriend(req, res) {};
+const getFriend = function getFriend(req, res) {
+  console.log("getFriend-req.loggedInUser ->", req.loggedInUser);
+  let user_id = req.loggedInUser.user_id;
+
+  const sqlTodos = `
+    select users.username, users.user_id, todos.todo from users
+    inner join
+      todos on users.user_id = todos.user_id
+    where 
+      users.username in (select friend_name from friends where user_id=?);
+`;
+
+  pool.execute(sqlTodos, [user_id], (error, result) => {
+    if (error) {
+      console.error(error);
+      res.sendStatus(500);
+      return;
+    } else {
+      console.log(result);
+      res.json(result).status(200);
+      return;
+    }
+  });
+};
 
 exports.getFriend = getFriend;
